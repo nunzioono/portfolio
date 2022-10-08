86c4d848f41e63f1df5723f4f6d299a79a286e8c
nunzioono <36959525+nunzioono@users.noreply.github.com>
2022-10-08 21:28:48 +0200

    done footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,25 +1,38 @@
+import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { selectDarkTheme } from '../slices/themeSlice';
+
 function Footer() {
+  
+  const darkTheme = useSelector(selectDarkTheme); 
+  const twitterPath = process.env.PUBLIC_URL+(darkTheme?"/images/footer_darktwitter.png":"/images/footer_twitter.png");
+  const instagramPath = process.env.PUBLIC_URL+(darkTheme?"/images/footer_darkinstagram.png":"/images/footer_instagram.png");
+  const linkedinPath = process.env.PUBLIC_URL+(darkTheme?"/images/footer_darklinkedin.png":"/images/footer_linkedin.png");
+  const githubPath = process.env.PUBLIC_URL+(darkTheme?"/images/footer_darkgithub.png":"/images/footer_github.png");
+  const dribbblePath = process.env.PUBLIC_URL+(darkTheme?"/images/footer_darkdribbble.png":"/images/footer_dribbble.png");
+  const behancePath = process.env.PUBLIC_URL+(darkTheme?"/images/footer_darkbehance.png":"/images/footer_behance.png");
+
 
   return (
-<div id="Footer">
-		<div id="n_2022_Nunzio_Onorati_All_righ">
-			<span>© 2022 Nunzio Onorati. All rights reserved.</span>
+	<div id="Footer" className="footer">
+		<div className="flex justify-between w-1/4">
+
+			<a href="https://twitter.com/NunzioonoDev" ><img src={twitterPath} alt="" className="footer_social_icon" /></a>
+
+			<a href="https://www.instagram.com/nunzio.o/" ><img src={instagramPath} alt="" className="footer_social_icon"/></a>
+
+			<a href="https://www.linkedin.com/in/nunzio-onorati-44b00015a/" ><img src={linkedinPath} alt="" className="footer_social_icon"/></a>
+
+			<a href="https://github.com/nunzioono" ><img src={githubPath} alt="" className="footer_social_icon"/></a>
+
+			<a href="https://dribbble.com/nunzioo" ><img src={dribbblePath} alt="" className="footer_social_icon"/></a>
+
+			<a href="https://www.behance.net/nunzioonorati" ><img src={behancePath} alt="" className="footer_social_icon"/></a>
+
 		</div>
-		<div id="social">
 
-			<img src="../svgs/footer_twitter.svg" alt="" />
-			
-			<img src="../svgs/footer_instagram.svg" alt="" />
-
-			<img src="../svgs/footer_linkedin.svg" alt="" />
-
-			<img src="../svgs/footer_github.svg" alt="" />
-
-			<img src="../svgs/footer_dribble.svg" alt="" />
-
-			<img src="../svgs/footer_behance.svg" alt="" />
-
-
+		<div className="font-martelbold text-lightblue text-xl dark:text-lightorange">
+			<span>© 2022 Nunzio Onorati. All rights reserved.</span>
 		</div>
 	</div>
   );
