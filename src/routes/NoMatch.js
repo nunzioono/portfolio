7e8c4e8c3a43c0ba014118f190a7a5de36f3afde
nunzioono <36959525+nunzioono@users.noreply.github.com>
2022-10-19 10:09:08 +0200

    done 404

--- src/routes/NoMatch.js
+++ src/routes/NoMatch.js
@@ -1,8 +1,11 @@
 const NoMatch= () => {
     return (
-        <h1>
-            No Match
-        </h1>
+        <div className="relative h-screen px-24 mt-[5.75rem] flex justify-center items-center dark:bg-dark">
+            <div className="flex flex-col justify-between">
+                <h1 className="font-cormorantbold text-center text-9xl text-lightblue dark:text-lightorange">404</h1>
+                <p className="font-cormorantregular text-center text-5xl text-grey dark:text-white">We are sorry, it seems the page do not belong to this website</p>
+            </div>
+        </div>
     );
 }
 
